87dbd2807bc569f1336ae57d5fb57c7e30b3cd46
UnBMaster <unbmaster@outlook.com>
2020-07-14 20:41:51 -0300

    Update 11

--- src/app/Domain/InscricaoService.php
+++ src/app/Domain/InscricaoService.php
@@ -58,7 +58,7 @@ class InscricaoService
                 $params['correlationId']
             );
 
-            var_dump($planos);
+           // var_dump($planos);
 
 
             # Lança exceção caso não haja dados remoto
